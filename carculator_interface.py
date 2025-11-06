d6b00c3c919c0e25760fe75b95ba49828d949657
Sky <Skykurakinkurakin@gmail.com>
2025-11-07 00:00:01 +0800

    Add file calculator_interface.py

---
+++ carculator_interface.py
@@ -0,0 +1,7 @@
+from calculator_processor import add 
+
+print("Welcome to calculator")
+number_1  = input("Enter first number: ")
+number_2 = input("Enter second2 number: ")
+
+print(f"the sum of your numbers is: {add(number_1,number_2)}")
